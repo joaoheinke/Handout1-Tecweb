2039c9474e4202b593ad523e2ba976f4e18ef0f9
joaoheinke <joaovtito@gmail.com>
2024-02-26 16:02:00 -0300

    handout 1

--- utils.py
+++ utils.py
@@ -12,3 +12,19 @@ def load_data(doc):
         conteudo = file.read()
         return json.loads(conteudo)
     
+def load_template(doc):
+    with open("templates/{0}".format(doc), 'r') as file:
+        return file.read()
+
+def save_data(doc, data):
+    with open("data/{0}".format(doc), 'w') as file:
+        file.write(json.dumps(data))
+
+def build_response(body='', code=200, reason='OK', headers=''):
+    response = f'HTTP/1.1 {code} {reason}\n'
+    response += f'Content-Length: {len(body)}\n'
+    response += 'Content-Type: text/html\n'
+    response += headers
+    response += '\n\n'
+    response = response + body
+    return response.encode()
